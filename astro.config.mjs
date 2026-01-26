c1ae3b0b0f607b829621ad78a631c57ab69be44a
yyoonchul <yiyc02@snu.ac.kr>
2026-01-26 21:11:10 +0900

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yiyoonchul.com',
+  site: 'https://yoonchulyi.com',
   integrations: [tailwind()],
 });
